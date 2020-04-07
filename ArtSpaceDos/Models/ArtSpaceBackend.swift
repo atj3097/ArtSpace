e10a3e8c4716132be0be0bb48eb23b2c43a0eacc
Adam <adamjackson@pursuit.org>
2020-04-07 13:50:43 -0400

    Backend url added

---
+++ ArtSpaceDos/Models/ArtSpaceBackend.swift
@@ -0,0 +1,13 @@
+//
+//  ArtSpaceBackend.swift
+//  ArtSpaceDos
+//
+//  Created by God on 4/7/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import Foundation
+
+struct ArtSpaceBackend {
+    static let backendURL: String = "https://artspace-7b89a.firebaseio.com/"
+}
